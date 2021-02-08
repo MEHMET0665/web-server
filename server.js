f31582cb5d5b481ef70b9c6fec5ea976374433d1
Memet <meavci@gmail.com>
2021-02-08 18:29:52 -0500

    create 2 file

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require("http");
 const host = 'localhost';
 const port = 8000;
-const requestListener = function (req, res) {
-  res.setHeader("Content-Type", "text/html");
-  res.end(`<html><body><h1>This is HTML</h1></body></html>`);
-  res.writeHead(200);
-  res.end("My first server!");
+
+  const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end("My first server!");
+    res.end(`<html><body><h1>This is HTML</h1></body></html>`);
+
 };
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
