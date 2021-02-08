b7fec4e7bdc0e17504a8d39691c2ac2f990caa52
Memet <meavci@gmail.com>
2021-02-08 18:14:13 -0500

    run localhost:8000

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 const requestListener = function (req, res) {
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Type", "text/html");
+  res.end(`<html><body><h1>This is HTML</h1></body></html>`);
   res.writeHead(200);
   res.end("My first server!");
 };
